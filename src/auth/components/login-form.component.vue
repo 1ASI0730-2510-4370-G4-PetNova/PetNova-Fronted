d0b9b15e1487b88d812546d550b3074ba2178b85
sebasepe <u202125968@upc.edu.pe>
2025-06-08 05:00:43 -0500

    feat(fronted): router

--- src/auth/components/login-form.component.vue
+++ src/auth/components/login-form.component.vue
@@ -38,14 +38,22 @@ const handleLogin = async () => {
   }
 
   try {
-    const users = await getUsers();
+    const users = await getUsers();  // Obtiene todos los usuarios desde la base de datos
     const foundUser = users.find(
-      user => user.email === email.value && user.password === password.value
+      user => user.email === email.value && user.password === password.value  // Busca el usuario que coincida con el correo y contraseña
     );
 
     if (foundUser) {
-      router.push('/clients');
+      // Si el rol es 'client', redirige a la página de clientes
+      if (foundUser.role === 'client') {
+        router.push('/profile');  // Redirige al dashboard del cliente
+      }
+      // Si el rol es 'admin' o 'vet', redirige a la misma página (por ejemplo, /admin-vet-dashboard)
+      else if (foundUser.role === 'admin' || foundUser.role === 'vet') {
+        router.push('/pets');  // Redirige a la página común para admin y vet
+      }
     } else {
+      console.error('Usuario o contraseña incorrectos');
     }
   } catch (error) {
     console.error('Error al iniciar sesión:', error);
